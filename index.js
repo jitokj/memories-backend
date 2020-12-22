340f6cfdb36c0d77ebda263ea6e8227f184b6e52
jito <jitokj3@gmail.com>
2020-12-22 16:13:53 +0530

    added server

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(bodyParser.json({limit: "30mb",extended: true}));
+app.use(bodyParser.urlencoded({limit: "30mb",extended: true}));
+app.use(cors());
+
+const CONNECTION_URL = "mongodb+srv://admin:xEpJD8ujXIcFqiEe@cluster0.ox0yc.mongodb.net/<dbname>?retryWrites=true&w=majority";
+const PORT = process.env.PORT || 5000;
+
+
+mongoose.connect(CONNECTION_URL,{useNewUrlParser: true,useUnifiedTopology: true})
+.then(()=>{
+    app.listen(PORT,()=>{
+        console.log(`Server listening on port:${PORT}`);
+    })
+})
+.catch(e=>{
+    console.log(e.message);
+});
+
+mongoose.set('useFindAndModify',false);
