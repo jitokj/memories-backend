a82971bfbee160c3a9620f2ef4e0c6327a6162b9
jito <jitokj3@gmail.com>
2020-12-28 15:11:05 +0530

    mvc pattern

--- index.js
+++ index.js
@@ -3,7 +3,11 @@ const cors = require('cors');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 
+const postRoutes = require('./routes/posts');
+
 const app = express();
+
+app.use('/posts',postRoutes);
 
 app.use(bodyParser.json({limit: "30mb",extended: true}));
 app.use(bodyParser.urlencoded({limit: "30mb",extended: true}));
