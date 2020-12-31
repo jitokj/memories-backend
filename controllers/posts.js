73652b4d9a6b2fe88ba702dc8f6409eae14d426c
jito <jitokj3@gmail.com>
2020-12-31 16:32:16 +0530

    delete route

--- controllers/posts.js
+++ controllers/posts.js
@@ -27,8 +27,21 @@ exports.updatePosts = async (req, res) => {
   if (!mongoose.Types.ObjectId.isValid(_id)) {
     return res.status(404).send("No post with that id");
   }
-  const updatedPost = await PostMessage.findByIdAndUpdate(_id, post, {
-    new: true,
-  });
+  const updatedPost = await PostMessage.findByIdAndUpdate(
+    _id,
+    { ...post, _id },
+    {
+      new: true,
+    }
+  );
   res.json(updatedPost);
+};
+
+exports.deletePost = async (req, res) => {
+  const { id } = req.params;
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(404).send("No post with that id");
+  }
+  await PostMessage.findByIdAndRemove(id);
+  res.json({ message: "post deleted succesfully" });
 };
--- routes/posts.js
+++ routes/posts.js
@@ -1,10 +1,17 @@
 const express = require("express");
 const router = express.Router();
-const { getPosts, createPosts, updatePosts } = require("../controllers/posts");
+const {
+  getPosts,
+  createPosts,
+  updatePosts,
+  deletePost,
+} = require("../controllers/posts");
 
 router.get("/", getPosts);
 router.post("/", createPosts);
 
 router.patch("/:id", updatePosts);
+
+router.delete("/:id", deletePost);
 
 module.exports = router;
